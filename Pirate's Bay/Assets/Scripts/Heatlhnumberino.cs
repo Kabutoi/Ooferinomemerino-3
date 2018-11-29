701496e05a58c64758f239e807e5bab1ef77dc9f
Kabutoi <alliwantinlifeisasenpai@gmail.com>
2018-11-28 20:38:08 -0600

    oof

---
+++ Pirate's Bay/Assets/Scripts/Heatlhnumberino.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Heatlhnumberino : MonoBehaviour {
+
+    public Text HealthNumber;
+
+    public GameObject Playerhealth;
+
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Healyh>.Health();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
